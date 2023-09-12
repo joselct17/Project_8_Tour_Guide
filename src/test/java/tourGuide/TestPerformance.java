2036d35f2796327859ea786f916a14020115166e
joselct17 <62754339+joselct17@users.noreply.github.com>
2023-09-12 12:50:11 +0200

    rajout de commentaires

--- src/test/java/tourGuide/TestPerformance.java
+++ src/test/java/tourGuide/TestPerformance.java
@@ -45,38 +45,6 @@ public class TestPerformance {
 	 */
 
 
-//	@Test
-//	public void highVolumeTrackLocation() {
-//		// Initialisation des services et des objets nécessaires
-//		GpsUtil gpsUtil = new GpsUtil();
-//		RewardsService rewardsService = new RewardsService(gpsUtil, new RewardCentral());
-//
-//		// Configuration des paramètres internes pour le test
-//		InternalTestHelper.setInternalUserNumber(100000);
-//
-//		// Initialisation du service TourGuideService
-//		TourGuideService tourGuideService = new TourGuideService(gpsUtil, rewardsService);
-//
-//		// Récupération de la liste de tous les utilisateurs
-//		List<User> allUsers = tourGuideService.getAllUsers();
-//
-//		// Initialisation du chronomètre
-//		StopWatch stopWatch = new StopWatch();
-//		stopWatch.start();
-//
-//		// Suivi de la localisation de tous les utilisateurs en parallèle et attente de la fin
-//		tourGuideService.trackUserLocation(allUsers).join();
-//
-//		// Arrêt du chronomètre et du suivi de localisation
-//		stopWatch.stop();
-//		tourGuideService.tracker.stopTracking();
-//
-//		// Affichage du temps écoulé pour le test
-//		System.out.println("highVolumeTrackLocation: Time Elapsed: " + TimeUnit.MILLISECONDS.toSeconds(stopWatch.getTime()) + " seconds.");
-//
-//		// Vérification que le temps écoulé est inférieur ou égal à 15 minutes
-//		assertTrue(TimeUnit.MINUTES.toSeconds(15) >= TimeUnit.MILLISECONDS.toSeconds(stopWatch.getTime()));
-//	}
 
 	@Test
 	public void highVolumeTrackLocation() throws InterruptedException {
@@ -108,58 +76,6 @@ public class TestPerformance {
 	}
 
 
-//	@Test
-//	public void highVolumeGetRewards() throws InterruptedException {
-//		// Initialisation des services et des objets nécessaires
-//		GpsUtil gpsUtil = new GpsUtil();
-//		RewardsService rewardsService = new RewardsService(gpsUtil, new RewardCentral());
-//
-//		// Configuration des paramètres internes pour le test
-//		InternalTestHelper.setInternalUserNumber(100000);
-//
-//		// Initialisation du chronomètre
-//		StopWatch stopWatch = new StopWatch();
-//		stopWatch.start();
-//
-//		// Initialisation du service TourGuideService
-//		TourGuideService tourGuideService = new TourGuideService(gpsUtil, rewardsService);
-//
-//		// Récupération de la première attraction depuis GpsUtil
-//		Attraction attraction = gpsUtil.getAttractions().get(0);
-//
-//		// Récupération de la liste de tous les utilisateurs
-//		List<User> allUsers = tourGuideService.getAllUsers();
-//
-//		// Chaque utilisateur visite l'attraction
-//		allUsers.forEach(u -> u.addToVisitedLocations(new VisitedLocation(u.getUserId(), attraction, new Date())));
-//
-//		// Calcul des récompenses pour chaque utilisateur
-//		allUsers.forEach(rewardsService::calculateRewards);
-//
-//		// Arrêt du service d'exécution des tâches de calcul des récompenses
-//		rewardsService.getExecutorService().shutdown();
-//
-//		// Attente de la fin de toutes les tâches pendant 19 minutes
-//		boolean allTasksIsFinished = rewardsService.getExecutorService().awaitTermination(19, TimeUnit.MINUTES);
-//
-//		// Affichage de l'état de toutes les tâches
-//		System.out.println("Tâches finies: " + allTasksIsFinished);
-//
-//		// Vérification que chaque utilisateur a reçu des récompenses
-//		for(User user : allUsers) {
-//			assertTrue(user.getUserRewards().size() > 0);
-//		}
-//
-//		// Arrêt du chronomètre et du suivi de localisation
-//		stopWatch.stop();
-//		tourGuideService.tracker.stopTracking();
-//
-//		// Affichage du temps écoulé pour le test
-//		System.out.println("highVolumeGetRewards: Time Elapsed: " + TimeUnit.MILLISECONDS.toSeconds(stopWatch.getTime()) + " seconds.");
-//
-//		// Vérification que le temps écoulé est inférieur ou égal à 20 minutes
-//		assertTrue(TimeUnit.MINUTES.toSeconds(20) >= TimeUnit.MILLISECONDS.toSeconds(stopWatch.getTime()));
-//	}
 
 
 	@Test
